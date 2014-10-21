2631649938250f32b8416f37585ae9adf03a390d
mkchoudhary86 <mkchoudhary.86@gmail.com>
2014-10-21 07:29:22 +0530

    branch1 commit
    Signed-off-by: mkchoudhary86 <mkchoudhary.86@gmail.com>

--- src/com/onmobile/QADND/HttpDND.java
+++ src/com/onmobile/QADND/HttpDND.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class HttpDND
  */
 
-//checking first change after initial commit
+//checking first change in branch1 commit
 
 @WebServlet("/HttpDND")
 public class HttpDND extends HttpServlet {
